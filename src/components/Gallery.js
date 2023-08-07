79d8149543af14a3114a6b6a9f51cbde294b3b7c
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-08-06 20:36:49 -0700

    finished suspense

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,11 +2,12 @@ import { useContext } from 'react';
 import { DataContext } from '../context/DataContext';
 import GalleryItem from './GalleryItem';
 
-function Gallery (props) {
+function Gallery () {
 
-    const data = props.data.result.read()
+    const data = useContext(DataContext)
+    const myData = data.result.read()
 
-    const display = props.data.map((song, i) => 
+    const display = myData.map((song, i) => 
     <GalleryItem song={song} key={i} />)
 
     return (
